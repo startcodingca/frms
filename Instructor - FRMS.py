2b4aaea6d442a453f79aac8a0eadcb066f90d677
startcodingca <107436735+startcodingca@users.noreply.github.com>
2022-06-13 15:54:50 -0700

    Add files via upload
    BECAUSE DIEGO DELETED HIS CODE.

--- Instructor - FRMS.py
+++ Instructor - FRMS.py
@@ -8,6 +8,8 @@ BLACK = (0,0,0)
 BLUE = (34,129,209)
 RED = (255,0,0)
 
+BALLSIZE = 25
+
 #starting up pygame
 print(pygame.init())
 
@@ -16,14 +18,38 @@ screen = pygame.display.set_mode((W, H))
 
 #BALL VARIABLES
 ballx = W/2 #400
-bally = H/2
+bally = 0
+
+ballspeedx = 0.2
+ballspeedy = 0.2
+
+p1x = 20
+p1y = H/2
+
+PADDLEW = 25
+PADDLEH = 100
 
 #game loop
 gameLoop = True
 while gameLoop == True:
   
-  ballx = ballx + 0.01
+  #BALL X BOUNCE
+  if ballx > W: #leaves right of screen
+    ballspeedx = -0.2
+  elif ballx < 0:
+    ballspeedx = 0.2
+  
+  ballx = ballx + ballspeedx
+  
+  #BALL Y BOUNCE
+  if bally > H:
+    ballspeedy = -0.2
+  elif ballx < 0:
+    ballspeedy = 0.2
+  
+  bally = bally + ballspeedy
   
   screen.fill(LGREY)
-  pygame.draw.rect(screen, RED, (ballx,bally,25,25))
+  pygame.draw.rect(screen, RED, (ballx,bally,BALLSIZE,BALLSIZE))
+  pygame.draw.rect(screen, BLUE, (p1x, p1y, PADDLEW, PADDLEH))
   pygame.display.update() #render
