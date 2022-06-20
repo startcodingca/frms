778a28a79e070a72d0a2ca8aa0d5987a7d0f9a25
startcodingca <107436735+startcodingca@users.noreply.github.com>
2022-06-20 13:43:54 -0700

    Add files via upload
    Jun13 Lesson Code

--- Instructor - FRMS.py
+++ Instructor - FRMS.py
@@ -20,11 +20,12 @@ screen = pygame.display.set_mode((W, H))
 ballx = W/2 #400
 bally = 0
 
-ballspeedx = 0.2
-ballspeedy = 0.2
+ballspeedx = 1
+ballspeedy = 1
 
 p1x = 20
 p1y = H/2
+p1speed = 0
 
 PADDLEW = 25
 PADDLEH = 100
@@ -32,20 +33,33 @@ PADDLEH = 100
 #game loop
 gameLoop = True
 while gameLoop == True:
+
+  for event in pygame.event.get():
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_w:
+        p1speed = -10
+      if event.key == pygame.K_s:
+        p1speed = 10
+
+    if event.type == pygame.KEYUP:
+      p1speed = 0
+  
+  #p1 movement
+  p1y = p1y + p1speed
   
   #BALL X BOUNCE
   if ballx > W: #leaves right of screen
-    ballspeedx = -0.2
+    ballspeedx = -1
   elif ballx < 0:
-    ballspeedx = 0.2
+    ballspeedx = 1
   
   ballx = ballx + ballspeedx
   
   #BALL Y BOUNCE
   if bally > H:
-    ballspeedy = -0.2
+    ballspeedy = -1
   elif ballx < 0:
-    ballspeedy = 0.2
+    ballspeedy = 1
   
   bally = bally + ballspeedy
   
