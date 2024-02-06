cf58993f8cc6d161dc5bb7421abf57ccdb9ee27f
startcodingca <107436735+startcodingca@users.noreply.github.com>
2024-02-06 15:57:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
 
 pygame.init( )
-
 screen = pygame.display.set_mode((800,600))
+clock = pygame.time.Clock()
+
+bird_y = 300
+yspeed = 0
 
 gamerunning = True
 while gamerunning == True:
   
+  for event in pygame.event.get():
+    if pygame.key.get_pressed()[pygame.K_SPACE]:
+      yspeed = -25
+
+  bird_y = bird_y + yspeed
+  yspeed += 1.65
+  
   screen.fill((204, 230, 255))
-  pygame.draw.rect(screen, (0,0,0), (0, 0, 400, 400))
+  pygame.draw.rect(screen, (0,0,0), (300, bird_y, 40, 40))
   pygame.display.update()
+  clock.tick(25)
   
