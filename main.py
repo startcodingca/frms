13c487cea623384091a800854e811ad2dac0cf38
startcodingca <107436735+startcodingca@users.noreply.github.com>
2024-02-20 14:57:09 -0800

    feb20 main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 
 pygame.init( )
 screen = pygame.display.set_mode((800,600))
@@ -6,19 +6,36 @@ clock = pygame.time.Clock()
 
 bird_y = 300
 yspeed = 0
+canJump = True
+
+pipe_x = 800
+pipe_y = random.randint(350, 550)
 
 gamerunning = True
 while gamerunning == True:
   
   for event in pygame.event.get():
-    if pygame.key.get_pressed()[pygame.K_SPACE]:
+    if pygame.key.get_pressed()[pygame.K_SPACE] and canJump == True:
       yspeed = -25
+      canJump = False
+
+    elif not pygame.key.get_pressed()[pygame.K_SPACE]:
+      canJump = True
 
   bird_y = bird_y + yspeed
   yspeed += 1.65
+
+  if pipe_x < -100:
+    pipe_x = 900
+    pipe_y = random.randint(350,550)
+
+  pipe_x -= 8
   
   screen.fill((204, 230, 255))
   pygame.draw.rect(screen, (0,0,0), (300, bird_y, 40, 40))
+  pygame.draw.rect(screen, (0,255,0), (pipe_x, pipe_y, 100, 600))
   pygame.display.update()
   clock.tick(25)
-  
+
+
+
